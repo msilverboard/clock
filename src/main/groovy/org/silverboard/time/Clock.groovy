9239ea34674d2c7c25af6f37c3451b33c85038a5
Marc Silverboard <msilverboard@cfglife.com>
2019-04-08 11:02:47 -0400

    modified:   build/libs/clock.jar 	new file:   clock.properties 	modified:   src/main/groovy/org/silverboard/time/Clock.groovy

--- src/main/groovy/org/silverboard/time/Clock.groovy
+++ src/main/groovy/org/silverboard/time/Clock.groovy
@@ -16,25 +16,43 @@ import org.apache.commons.net.ntp.NTPUDPClient
 import org.apache.commons.net.ntp.NtpV3Packet
 import org.apache.commons.net.ntp.TimeInfo
 
+// List of time servers: http://tf.nist.gov/tf-cgi/servers.cgi
+// Do not query time server more than once every 4 seconds
 class Clock {
 
-    // List of time servers: http://tf.nist.gov/tf-cgi/servers.cgi
-    // Do not query time server more than once every 4 seconds
-    private static final String TIME_SERVER = 'time-d-g.nist.gov'
+    private static final int MINUTE_IN_MILLIS = 60 * 000
+    private static final int HOUR_IN_MILLIS = 60 * MINUTE_IN_MILLIS
+    private static final List<String> STRING_PROPS = ['timeServer','format', 'fontName']
+    private static final List<String> INT_PROPS = ['windowWidth', 'windowHeight', 'backgroundColorR',
+        'backgroundColorG', 'backgroundColorB', 'foregroundColorR', 'foregroundColorG', 
+        'foregroundColorB', 'fontStyle', 'fontSize']
 
-    private static final int MINUTE_IN_MILLIS = 60000
-
-    private Date time
+    private Date currentTime
+    private Date lastSync
     private JFrame jFrame
     private JLabel timeLabel
 
-    /*
-     The clock takes its initial setting from NIST and resynchronizes every 15 minutes.
-     */
+    private String timeServer = 'time-d-g.nist.gov'
+    private String format = 'M/d/yyyy h:mm a z'
+    private String fontName = 'System 12pt'
+    private int fontStyle = Font.PLAIN
+    private int fontSize = 18
+    private int windowWidth = 300
+    private int windowHeight = 100
+    private int backgroundColorR = 0
+    private int backgroundColorG = 0
+    private int backgroundColorB = 0
+    private int foregroundColorR = 0
+    private int foregroundColorG = 128
+    private int foregroundColorB = 0
+
+    Clock() {
+        initClock()
+    }
+
     static void main(String [] args) {
         Clock clockInstance = new Clock()
-        clockInstance.initClock()
-        Thread.sleep(clockInstance.millisToNextMinue())
+        Thread.sleep(clockInstance.millisToNextMinute())
 
         while(true) {
             Integer waitTime = clockInstance.syncTime()
@@ -44,29 +62,60 @@ class Clock {
     }
 
     void initClock() {
+        readProperties()
         initializeWindow()
         syncNistTime()
         update()
     }
 
+    void readProperties() {
+        Properties props = new Properties()
+        String userHome = System.getProperty('user.home')
+        File file = new File("${userHome}/clock.properties")
+        if (file.exists()) {
+            props.load(file.newInputStream())
+        }
+
+        STRING_PROPS.each { propName ->
+            if (props && props[propName] != null) {
+                this[propName] = props[propName]
+            }
+        }
+
+        INT_PROPS.each { propName ->
+            if (props && props[propName] != null) {
+                this[propName] = Integer.parseInt(props[propName])
+            }
+        }
+    }
+
     void syncNistTime() {
         NTPUDPClient timeClient = new NTPUDPClient()
-        InetAddress inetAddress = InetAddress.getByName(TIME_SERVER)
+        InetAddress inetAddress = InetAddress.getByName(timeServer)
         TimeInfo timeInfo = timeClient.getTime(inetAddress)
         NtpV3Packet message = timeInfo.getMessage()
         long serverTime = message.getTransmitTimeStamp().getTime()
-        time = new Date(serverTime)
+        currentTime = new Date(serverTime)
+        lastSync = new Date(serverTime)
     }
 
     void initializeWindow() {
+        Color backgroundColor = new Color(
+            backgroundColorR,
+            backgroundColorG,
+            backgroundColorB)
+        Color foregroundColor = new Color(
+            foregroundColorR,
+            foregroundColorG,
+            foregroundColorB)
         jFrame = new JFrame('Atomic Time')
         jFrame.defaultCloseOperation = JFrame.EXIT_ON_CLOSE
-        jFrame.getContentPane().setBackground(Color.BLACK)
+        jFrame.getContentPane().setBackground(backgroundColor)
         jFrame.setSize(300, 100)
         timeLabel = new JLabel('', SwingConstants.CENTER)
-        timeLabel.preferredSize = new Dimension(300, 100)
-        timeLabel.font = new FontUIResource('System 12pt', Font.PLAIN, 18)
-        timeLabel.setForeground(Color.GREEN)
+        timeLabel.preferredSize = new Dimension(windowWidth, windowHeight)
+        timeLabel.font = new FontUIResource(fontName, fontStyle, fontSize)
+        timeLabel.setForeground(foregroundColor)
         jFrame.contentPane.add(timeLabel, BorderLayout.CENTER)
         jFrame.pack()
         // Position lower right
@@ -80,31 +129,32 @@ class Clock {
     }
 
     void update() {
-        timeLabel.setText(time.format('M/d/yyyy h:mm:ss a z'))
+        timeLabel.setText(currentTime.format(format))
     }
 
     Integer syncTime() {
         Integer waitTime
         Calendar calendar = GregorianCalendar.getInstance()
-        calendar.setTime(time)
+        calendar.setTime(currentTime)
         Integer minutes = calendar.get(Calendar.MINUTE)
-        if (minutes % 15 == 0) {
+        if (currentTime.time > (lastSync.time + (12 * HOUR_IN_MILLIS))) {
             syncNistTime()
             waitTime = millisToNextMinute()
         } else {
             calendar.add(Calendar.MINUTE, 1)
-            time = calendar.time
+            currentTime = calendar.time
             waitTime = MINUTE_IN_MILLIS
         }
         return waitTime
     }
 
-    Integer millisToNextMinue() {
+    Long millisToNextMinute() {
         Calendar calendar = GregorianCalendar.getInstance()
-        calendar.setTime(time)
+        calendar.setTime(currentTime)
         Integer seconds = calendar.get(Calendar.SECOND)
         Integer milliseconds = calendar.get(Calendar.MILLISECOND)
         Integer waitTime = ((60 - seconds) * 1000) + (1000 - milliseconds)
-        return waitTime
+        Long result = new Long(waitTime.intValue())
+        return result
     }
 }
